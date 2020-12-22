03082f802812a29629f8be32e146ff182a1e710e
baurelius <baurelius@protonmail.com>
2020-12-21 19:58:44 -0700

    0.1.1, fix setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@ from setuptools import setup
 
 setup(
     name='hivejdbc',
-    version='0.1.1',
+    version='0.1.2',
     author='OpenBigDataPlatform',
-    license='Apache 2',
+    license='Apache-2.0',
     url='https://github.com/OpenBigDataPlatform/hivejdbc',
     description=('Hive database driver via jdbc'),
     long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     keywords=('dbapi jdbc hive hadoop'),
     classifiers=[
         'Development Status :: 4 - Beta',
@@ -21,6 +22,6 @@ setup(
 
     packages=['hivejdbc'],
     install_requires=[
-        'pyjdbc>=0.1.1'
+        'pyjdbc>=0.1.2'
     ],
 )
