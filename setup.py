f9bf4025285cc6f272c133959c6cb687ce7d515b
baurelius <baurelius@protonmail.com>
2020-12-21 20:16:46 -0700

    bump version, setup.py typos

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup
 
 setup(
     name='hivejdbc',
-    version='0.1.2',
+    version='0.1.3',
     author='OpenBigDataPlatform',
     license='Apache-2.0',
     url='https://github.com/OpenBigDataPlatform/hivejdbc',
-    description=('Hive database driver via jdbc'),
+    description='Hive database driver via jdbc',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    keywords=('dbapi jdbc hive hadoop'),
+    keywords='dbapi jdbc hive hadoop',
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
@@ -22,6 +22,6 @@ setup(
 
     packages=['hivejdbc'],
     install_requires=[
-        'pyjdbc>=0.1.2'
+        'pyjdbc>=0.1.3'
     ],
 )
